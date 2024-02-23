09d973df071a17e13d8041079e554fbbdb6eee9a
San Manyike <san.manyike@thedatacompany.com>
2024-02-23 15:47:05 +0200

    Update: Improve CSV data exports

--- my_livewire_app/app/Exports/CsvExport.php
+++ my_livewire_app/app/Exports/CsvExport.php
@@ -3,14 +3,22 @@
 namespace App\Exports;
 
 use Maatwebsite\Excel\Concerns\FromCollection;
+use Illuminate\Support\Collection;
 
 class CsvExport implements FromCollection
 {
+    protected $model;
+
+    public function __construct(Collection $model)
+    {
+        $this->model = $model;
+    }
+
     /**TODO
     * @return \Illuminate\Support\Collection
     */
     public function collection()
     {
-        //
+        return $this->model;
     }
 }
